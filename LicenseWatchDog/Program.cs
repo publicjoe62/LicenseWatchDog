3e60964dfd974343c99b83cbcc71438fde94eeda
Joe Public <publicjoe62@gmail.com>
2018-12-18 13:19:14 +0100

    Initial documents
    push initial documents

---
+++ LicenseWatchDog/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LicenseWatchDog
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MainClass mc = new MainClass();
+
+            Console.WriteLine("License Watch Dog Manager :: start app");
+            mc.ExecTask();
+            Console.WriteLine("License Watch Dog Manager :: end app");
+
+        }
+
+        private void ExecTask()
+        {
+            Task taskA = Task.Run(() => LicenseTask());
+            Console.WriteLine("taskA Status: {0}", taskA.Status);
+            try
+            {
+                taskA.Wait();
+                Console.WriteLine("taskA Status: {0}", taskA.Status);
+            }
+            catch (AggregateException)
+            {
+                Console.WriteLine("Exception in taskA");
+            }
+        }
+
+        private void LicenseTask()
+        {
+            while (true)
+            {
+                Thread.Sleep(2000);
+                Console.Write(".");
+            }
+        }
+
+    }
+}
